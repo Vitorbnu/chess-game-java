7a36f59b4c97e5c7434db3e7785e2e7c82fbba55
Vitorbnu <72954077+Vitorbnu@users.noreply.github.com>
2023-09-19 22:32:43 -0300

    Queen

--- chess-java/src/chess/ChessMatch.java
+++ chess-java/src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -202,6 +203,7 @@ public class ChessMatch {
 		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 		placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
@@ -218,11 +220,12 @@ public class ChessMatch {
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 		placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('g', 8, new Knight(board, Color.WHITE));
+		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
 	}
 }
--- chess-java/src/chess/pieces/Knight.java
+++ chess-java/src/chess/pieces/Knight.java
@@ -25,7 +25,7 @@ public class Knight extends ChessPiece {
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		Position p = new Position(0, 0);
-//Above
+
 		p.setValues(position.getRow() - 1, position.getColumn() - 2);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
@@ -64,8 +64,8 @@ public class Knight extends ChessPiece {
 			if (getBoard().positionExists(p) && canMove(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			return mat;
 		}
+		return mat;
 
 	}
 }
